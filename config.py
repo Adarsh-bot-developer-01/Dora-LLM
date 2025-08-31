2e1cb3e1f97611cfb6afa9611f4945244c66c30a
Adarsh-bot-developer-01 <jaiswaladarsh2009@gmail.com>
2025-08-31 17:06:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,13 @@
+import os
 
+# 🔹 Bot Config
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+OWNER_ID = int(os.getenv("OWNER_ID"))
+FORCE_CHANNEL = os.getenv("FORCE_CHANNEL")
+FORCE_GROUP = os.getenv("FORCE_GROUP")
+
+# 🔹 Bot Version
+BOT_VERSION = "1.0.0"
+
+# 🔹 Special Logging Group (future use)
+DEV_LOG_GROUP = os.getenv("DEV_LOG_GROUP", OWNER_ID)
